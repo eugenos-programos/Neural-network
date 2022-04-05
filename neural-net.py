575f43bd76908888a7e15c1291c10e24ece13278
eugenos-programos <elena.samchuk.73@mail.ru>
2022-04-05 18:08:47 +0300

    Fix one more bug in forward propagation function

--- neural-net.py
+++ neural-net.py
@@ -4,7 +4,6 @@ from types import LambdaType
 
 
 class NeuralNetwork():
-
     activation_func: LambdaType
     activation_func_derivative: LambdaType
     alpha: int
@@ -31,16 +30,16 @@ class NeuralNetwork():
         """
         self.L = L
         self.activation_func, self.activation_func_derivative = get_function_and_derivative(activation)
-        if neuron_number_list and len(neuron_number_list) != L:
+        layer_count = neuron_number_list.shape[0]
+        if layer_count and layer_count != L:
             raise ValueError(
                 "Neuron number list length should be equal to L parameter")
-        if (not neuron_number_list
-                and not neuron_number) or (neuron_number_list
-                                           and neuron_number):
-            raise BaseException(
+        if (not layer_count
+            and not neuron_number) or (layer_count and neuron_number):
+            raise ValueError(
                 "Should be initialized one of such parameters: neuron_number_list and neuron_number"
             )
-        if not neuron_number_list:
+        if not layer_count:
             neuron_number_list = [neuron_number for _ in range(L)]
         self.parameters = {}
         self.neurons_number = neuron_number_list
@@ -55,24 +54,26 @@ class NeuralNetwork():
                number_neurons_values: np.array = np.array([])):
         """
         Append N new layers to the neural network
-        :param N: int 
+        :param number_neurons_values:
+        :param activations_list:
+        :param N: int
             number of new layers
         :param n_neurons: int
             number of the neurons for each layer
         :param activation: str
-            activation function name for each layer 
+            activation function name for each layer
             possible values - {"ReLU", "sigmoid", "tanh"}
             default - ReLU
         """
         if number_neurons_values and len(number_neurons_values) != N:
-            raise BaseException("List length doesn't correspond to the N parameter value: {} != {}".\
-                                format(len(number_neurons_values), N))
+            raise ValueError("List length doesn't correspond to the N parameter value: {} != {}".\
+                             format(len(number_neurons_values), N))
         if activations_list and len(activations_list) != N:
-            raise BaseException("Actiavation list length doesn't correspond to the N parameter value: {} != {}".\
-                                format(len(activations_list), N))
+            raise ValueError("Activation list length doesn't correspond to the N parameter value: {} != {}".\
+                             format(len(activations_list), N))
         pass
 
-    def calculate_loss(X: np.array = None, y:np.array = None, data: np.array = None):
+    def calculate_loss(X: np.array = None, y: np.array = None, data: np.array = None):
         pass
 
     def initialize_weights(self) -> None:
@@ -94,7 +95,7 @@ class NeuralNetwork():
         if not isinstance(l, int):
             raise TypeError("l parameter should be int type")
         if l <= 0:
-            raise BaseException(
+            raise ValueError(
                 "Uncorrect l value - {}. Must be not negative.".format(l))
         W = np.random.randn(self.neurons_number[l],
                             self.neurons_number[l - 1]) * 0.01
@@ -121,21 +122,19 @@ class NeuralNetwork():
         m = len(y)
         for index in range(m):
             x_ = X[index]
-            print(x_.shape)
-            outp, cache = self.predict(x_, return_activation_cache=True) # (1, 5)
-            outp = outp.T # (5, 1)
-            dZ = outp - y # (5, 1)
-            print(outp.shape, dZ.shape)
-            dW = (1 / m) * (dZ @ outp) # (5, 5)
-            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True) # (5, 1)
+            outp, cache = self.predict(x_, return_activation_cache=True)  # (5, 1)
+            print(outp.shape, x_.shape)
+            dZ = outp - y  # (5, 1)
+            dW = (1 / m) * (dZ @ outp.T)  # (5, 5)
+            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)  # (5, 1)
             W = self.parameters["W{}".format(self.L - 1)]  # (1, 5)
-            self.parameters["W{}".format(self.L - 1)] -= self.alpha * dW.T
+            self.parameters["W{}".format(self.L - 1)] -= self.alpha * dW
             self.parameters["b{}".format(self.L - 1)] -= self.alpha * db
             for layer_index in range(self.L - 1, 1, -1):
                 print(layer_index)
                 Z = cache["Z{}".format(layer_index)]
                 dZ = (W.T @ dZ) @ self.activation_func_derivative(Z)
-                dW = (1 / m) * (dZ @ Z.T)   
+                dW = (1 / m) * (dZ @ Z.T)
                 db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
                 self.parameters["W{}".format(layer_index)] -= self.alpha * dW
                 self.parameters["b{}".format(layer_index)] -= self.alpha * db
@@ -153,17 +152,21 @@ class NeuralNetwork():
                 y, Z - predicted target value and activations in each layer
                 when return_activation_cache parameter is true 
         """
-        if self.parameters["W1"].shape[1] != X.shape[1]:
-            raise ValueError("X shape doesn't correspond to weight shape on first layer")
+        if isinstance(X, list):
+            TypeError("X parameter should be ndarray type")
+        if len(X.shape) == 1:
+            X = X.reshape(1, X.shape[0])
+        try:
+            Z = np.dot(self.parameters["W1"], X.T)
+        except Exception as e:
+            raise ValueError("X shape doesn't corresponding the the neural-net first layer size")
         Z = X.T
-        print(Z.shape)
         cache_data = {}
         for layer_index in range(1, self.L):
             W = self.parameters["W{}".format(layer_index)]
             b = self.parameters["b{}".format(layer_index)]
             A = np.dot(W, Z) + b
             Z = self.activation_func(A)
-            print(Z.shape)
             if return_activation_cache:
                 cache_data["Z{}".format(layer_index)] = Z
         Z = Z.T
@@ -172,9 +175,7 @@ class NeuralNetwork():
         return Z
 
 
-nn = NeuralNetwork(5, neuron_number_list=[4, 5, 5, 5, 1], activation='ReLU') 
-X = np.random.rand(145, 4)
+nn = NeuralNetwork(5, neuron_number_list=np.array([4, 5, 5, 5, 1]), activation='ReLU')
+X = np.random.rand(4, )
 y = np.random.rand(5, 1)
-print("Shape - {}".format(nn.predict(X).shape))
-
-
+nn.fit(X).shape
